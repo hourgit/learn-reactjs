7d96bef3058540f6a0a3541689a04a2bd18a0a23
Haimenghour <bophakok0700@gmail.com>
2020-07-05 19:23:10 +0700

    fix

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+
+function Main() {
+  return (
+    <>
+      <Navigation />
+      <h1>Learn-reatjs</h1>
+    </>
+    
+  );
+}
+
+export default Main;
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List(){
+    return(
+        <>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Home</li>
+        </>
+    )
+}
+
+export default List;
